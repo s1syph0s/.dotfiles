793b18cd3255ba72a42af23e43ebfc8ede2c4c76
Pasha Fistanto <pasha.fistanto@gmail.com>
2023-08-25 20:22:40 +0200

    Refactor

--- nvim/lua/custom/plugins/oil-setup.lua
+++ nvim/lua/custom/plugins/oil-setup.lua
@@ -1,29 +1,31 @@
+local config = function()
+	require('oil').setup({
+		-- Set to false to disable all of the above keymaps
+		use_default_keymaps = false,
+		keymaps = {
+			["g?"] = "actions.show_help",
+			["<CR>"] = "actions.select",
+			["<C-s>"] = "actions.select_vsplit",
+			["<C-b>"] = "actions.select_split",
+			["<C-t>"] = "actions.select_tab",
+			["<C-p>"] = "actions.preview",
+			["<C-c>"] = "actions.close",
+			["gr"] = "actions.refresh",
+			["-"] = "actions.parent",
+			["_"] = "actions.open_cwd",
+			["`"] = "actions.cd",
+			["~"] = "actions.tcd",
+			["g."] = "actions.toggle_hidden",
+		},
+	})
+end
+
 return {
 	{
 		'stevearc/oil.nvim',
 		opts = {},
 		-- Optional dependencies
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require('oil').setup({
-				-- Set to false to disable all of the above keymaps
-				use_default_keymaps = false,
-				keymaps = {
-					["g?"] = "actions.show_help",
-					["<CR>"] = "actions.select",
-					["<C-s>"] = "actions.select_vsplit",
-					["<C-b>"] = "actions.select_split",
-					["<C-t>"] = "actions.select_tab",
-					["<C-p>"] = "actions.preview",
-					["<C-c>"] = "actions.close",
-					["gr"] = "actions.refresh",
-					["-"] = "actions.parent",
-					["_"] = "actions.open_cwd",
-					["`"] = "actions.cd",
-					["~"] = "actions.tcd",
-					["g."] = "actions.toggle_hidden",
-				},
-			})
-		end
+		config = config,
 	},
 }
