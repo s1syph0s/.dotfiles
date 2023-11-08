e4b4bbbdd64ce5859909b58bddbb92cd68ad6b1c
Pasha Fistanto <pasha.fistanto@gmail.com>
2023-11-09 00:16:55 +0100

    Install texlab

--- nvim/init.lua
+++ nvim/init.lua
@@ -479,9 +479,10 @@ local servers = {
   -- clangd = {},
   -- gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
+  rust_analyzer = {},
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  texlab = {},
 
   lua_ls = {
     Lua = {
