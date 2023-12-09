252b74762a18074e5f9a287cf49b803a5d3163cf
Pasha Fistanto <pasha.fistanto@gmail.com>
2023-12-09 13:13:17 +0100

    Luasnip tab bug fix

--- nvim/init.lua
+++ nvim/init.lua
@@ -407,6 +407,26 @@ local luasnip = require 'luasnip'
 require('luasnip.loaders.from_vscode').lazy_load()
 luasnip.config.setup {}
 
+local unlinkgrp = vim.api.nvim_create_augroup(
+  'UnlinkSnippetOnModeChange',
+  { clear = true }
+)
+
+vim.api.nvim_create_autocmd('ModeChanged', {
+  group = unlinkgrp,
+  pattern = { 's:n', 'i:*' },
+  desc = 'Forget the current snippet when leaving the insert mode',
+  callback = function(evt)
+    if
+        luasnip.session
+        and luasnip.session.current_nodes[evt.buf]
+        and not luasnip.session.jump_active
+    then
+      luasnip.unlink_current()
+    end
+  end,
+})
+
 cmp.setup {
   snippet = {
     expand = function(args)
