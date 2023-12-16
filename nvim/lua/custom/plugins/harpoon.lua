272f3ceb26fcfaf2a52aaa433ef469244a0c1e33
Pasha Fistanto <pasha.fistanto@gmail.com>
2023-12-16 22:08:59 +0100

    Give whichkey descriptions

--- nvim/lua/custom/plugins/harpoon.lua
+++ nvim/lua/custom/plugins/harpoon.lua
@@ -11,12 +11,12 @@ return {
 		harpoon:setup()
 		-- REQUIRED
 
-		vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
-		vim.keymap.set("n", "<leader>hm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+		vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end, { desc = '[H]arpoon [A]ppend' })
+		vim.keymap.set("n", "<leader>ht", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = '[H]arpoon [T]oggle menu' })
 
-		vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-		vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-		vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-		vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
+		vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = 'Goto Harpoon 1st' })
+		vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = 'Goto Harpoon 2nd' })
+		vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = 'Goto Harpoon 3rd' })
+		vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = 'Goto Harpoon 4th' })
 	end
 }
