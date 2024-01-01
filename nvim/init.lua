2d56b4bd53a35e4544a7c22ccce1669b960f3c2a
Pasha Fistanto <pasha.fistanto@gmail.com>
2024-01-01 11:38:19 +0100

    new theme and update

--- nvim/init.lua
+++ nvim/init.lua
@@ -157,8 +157,16 @@ require('lazy').setup({
     lazy = false,
     priority = 1000,
     opts = {},
+    -- config = function()
+    --   vim.cmd.colorscheme 'tokyonight'
+    -- end,
+  },
+
+  {
+    'rebelot/kanagawa.nvim',
+    priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'tokyonight'
+      vim.cmd.colorscheme 'kanagawa'
     end,
   },
 
